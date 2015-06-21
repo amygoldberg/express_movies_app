924275b2c732a29dc25299ffe5048af60dc565ef
amygoldberg <amykgoldberg@gmail.com>
2015-06-21 13:12:07 -0400

    starting writing the movie schema

--- app.js
+++ app.js
@@ -4,6 +4,18 @@ var express = require('express');
 // 2 create an instance of an express application
 var app = express();
 
+// 4 create an api router to make get requests more dry
+var apiRouter = express.Router();
+
+// 6 create a GET request for '/movies' using apiRouter
+apiRouter.get('/movies', function(req, res) {
+  Movie.find({}, function(error, movieList) {
+    res.json(movieList);
+  });
+});
+
+// 5 mount the apiRouter defined above onto our instance of express
+app.use('/api', apiRouter);
 
 // 3 start the server
 // get the internet address the server is listening on
---
+++ scripts/seed-movies-database.js
@@ -0,0 +1,7 @@
+// Your mission is to set up a movie review database.
+
+// Movies have a lot of data associated with them! They have a title, a year of production, a director, and at least one actor. They also have a genre (which fr our purposes is limited to action, comedy, crime, drama, suspense, horror, romance, science fiction, fantasy, war, and western) and a rating (G, PG, PG-13, R, NC-17, Foreign, Unrated, or Other).
+
+// Most movies will have at least one review. Some will have several, and a few will have many. Reviews have a title, an reviewer, a star rating, a publication date, and a body.
+
+// In the first phase of this project, you should create a Mongoose schema and model object according to this description, and some database seed data to verify that the schema and validation work.
