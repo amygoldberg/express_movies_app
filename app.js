4d088213f8075a0d866fdbbc5cae4098ba8c62f3
amygoldberg <amykgoldberg@gmail.com>
2015-06-21 12:37:02 -0400

    setting up the app, requiring modules needed and starting the server listening

--- app.js
+++ app.js
@@ -1,2 +1,14 @@
+// 1 require express
 var express = require('express');
+
+// 2 create an instance of an express application
 var app = express();
+
+
+// 3 start the server
+// get the internet address the server is listening on
+var server = app.listen(3000, function() {
+  var host = server.address().address;
+  var port = server.address().port;
+  console.log("Movie app listening at http://%s:%s", host, port);
+});
